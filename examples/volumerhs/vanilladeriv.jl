e305e5d105a9858b163e2b2b5d28b99da6e03eed
Lucas C Wilcox <lucas@swirlee.com>
2019-04-25 23:53:48 +0000

    Update volumerhs to latest packages

--- examples/volumerhs/vanilladeriv.jl
+++ examples/volumerhs/vanilladeriv.jl
@@ -305,129 +305,129 @@ function volumerhs_v3!(::Val{3},
             @loop for i in (1:Nq; threadIdx().x)
                  for k in 1:Nq
                      r_rhsρ[k, i, j] = zero(eltype(rhs))
-                #     r_rhsU[k, i, j] = zero(eltype(rhs))
-                #     r_rhsV[k, i, j] = zero(eltype(rhs))
-                #     r_rhsW[k, i, j] = zero(eltype(rhs))
-                #     r_rhsE[k, i, j] = zero(eltype(rhs))
+                     r_rhsU[k, i, j] = zero(eltype(rhs))
+                     r_rhsV[k, i, j] = zero(eltype(rhs))
+                     r_rhsW[k, i, j] = zero(eltype(rhs))
+                     r_rhsE[k, i, j] = zero(eltype(rhs))
                 end
 
                 # fetch D into shared
-                # s_D[i, j] = D[i, j]
+                s_D[i, j] = D[i, j]
             end
         end
 
-        # @unroll for k in 1:Nq
-        #     @synchronize
-        #     @loop for j in (1:Nq; threadIdx().y)
-        #         @loop for i in (1:Nq; threadIdx().x)
+        @unroll for k in 1:Nq
+            @synchronize
+            @loop for j in (1:Nq; threadIdx().y)
+                @loop for i in (1:Nq; threadIdx().x)
 
-        #             # Load values will need into registers
-        #             MJ = vgeo[i, j, k, _MJ, e]
-        #             ξx, ξy, ξz = vgeo[i,j,k,_ξx,e], vgeo[i,j,k,_ξy,e], vgeo[i,j,k,_ξz,e]
-        #             ηx, ηy, ηz = vgeo[i,j,k,_ηx,e], vgeo[i,j,k,_ηy,e], vgeo[i,j,k,_ηz,e]
-        #             ζx, ζy, ζz = vgeo[i,j,k,_ζx,e], vgeo[i,j,k,_ζy,e], vgeo[i,j,k,_ζz,e]
-        #             z = vgeo[i,j,k,_z,e]
+                    # Load values will need into registers
+                    MJ = vgeo[i, j, k, _MJ, e]
+                    ξx, ξy, ξz = vgeo[i,j,k,_ξx,e], vgeo[i,j,k,_ξy,e], vgeo[i,j,k,_ξz,e]
+                    ηx, ηy, ηz = vgeo[i,j,k,_ηx,e], vgeo[i,j,k,_ηy,e], vgeo[i,j,k,_ηz,e]
+                    ζx, ζy, ζz = vgeo[i,j,k,_ζx,e], vgeo[i,j,k,_ζy,e], vgeo[i,j,k,_ζz,e]
+                    z = vgeo[i,j,k,_z,e]
 
-        #             U, V, W = Q[i, j, k, _U, e], Q[i, j, k, _V, e], Q[i, j, k, _W, e]
-        #             ρ, E = Q[i, j, k, _ρ, e], Q[i, j, k, _E, e]
+                    U, V, W = Q[i, j, k, _U, e], Q[i, j, k, _V, e], Q[i, j, k, _W, e]
+                    ρ, E = Q[i, j, k, _ρ, e], Q[i, j, k, _E, e]
 
-        #             P = gdm1*(E - (U^2 + V^2 + W^2)/(2*ρ) - ρ*gravity*z)
+                    P = gdm1*(E - (U^2 + V^2 + W^2)/(2*ρ) - ρ*gravity*z)
 
-        #             ρinv = 1 / ρ
+                    ρinv = 1 / ρ
 
-        #             fluxρ_x = U
-        #             fluxU_x = ρinv * U * U + P
-        #             fluxV_x = ρinv * U * V
-        #             fluxW_x = ρinv * U * W
-        #             fluxE_x = ρinv * U * (E + P)
+                    fluxρ_x = U
+                    fluxU_x = ρinv * U * U + P
+                    fluxV_x = ρinv * U * V
+                    fluxW_x = ρinv * U * W
+                    fluxE_x = ρinv * U * (E + P)
 
-        #             fluxρ_y = V
-        #             fluxU_y = ρinv * V * U
-        #             fluxV_y = ρinv * V * V + P
-        #             fluxW_y = ρinv * V * W
-        #             fluxE_y = ρinv * V * (E + P)
+                    fluxρ_y = V
+                    fluxU_y = ρinv * V * U
+                    fluxV_y = ρinv * V * V + P
+                    fluxW_y = ρinv * V * W
+                    fluxE_y = ρinv * V * (E + P)
 
-        #             fluxρ_z = W
-        #             fluxU_z = ρinv * W * U
-        #             fluxV_z = ρinv * W * V
-        #             fluxW_z = ρinv * W * W + P
-        #             fluxE_z = ρinv * W * (E + P)
+                    fluxρ_z = W
+                    fluxU_z = ρinv * W * U
+                    fluxV_z = ρinv * W * V
+                    fluxW_z = ρinv * W * W + P
+                    fluxE_z = ρinv * W * (E + P)
 
-        #             s_F[i, j,  _ρ] = MJ * (ξx * fluxρ_x + ξy * fluxρ_y + ξz * fluxρ_z)
-        #             s_F[i, j,  _U] = MJ * (ξx * fluxU_x + ξy * fluxU_y + ξz * fluxU_z)
-        #             s_F[i, j,  _V] = MJ * (ξx * fluxV_x + ξy * fluxV_y + ξz * fluxV_z)
-        #             s_F[i, j,  _W] = MJ * (ξx * fluxW_x + ξy * fluxW_y + ξz * fluxW_z)
-        #             s_F[i, j,  _E] = MJ * (ξx * fluxE_x + ξy * fluxE_y + ξz * fluxE_z)
+                    s_F[i, j,  _ρ] = MJ * (ξx * fluxρ_x + ξy * fluxρ_y + ξz * fluxρ_z)
+                    s_F[i, j,  _U] = MJ * (ξx * fluxU_x + ξy * fluxU_y + ξz * fluxU_z)
+                    s_F[i, j,  _V] = MJ * (ξx * fluxV_x + ξy * fluxV_y + ξz * fluxV_z)
+                    s_F[i, j,  _W] = MJ * (ξx * fluxW_x + ξy * fluxW_y + ξz * fluxW_z)
+                    s_F[i, j,  _E] = MJ * (ξx * fluxE_x + ξy * fluxE_y + ξz * fluxE_z)
 
-        #             s_G[i, j,  _ρ] = MJ * (ηx * fluxρ_x + ηy * fluxρ_y + ηz * fluxρ_z)
-        #             s_G[i, j,  _U] = MJ * (ηx * fluxU_x + ηy * fluxU_y + ηz * fluxU_z)
-        #             s_G[i, j,  _V] = MJ * (ηx * fluxV_x + ηy * fluxV_y + ηz * fluxV_z)
-        #             s_G[i, j,  _W] = MJ * (ηx * fluxW_x + ηy * fluxW_y + ηz * fluxW_z)
-        #             s_G[i, j,  _E] = MJ * (ηx * fluxE_x + ηy * fluxE_y + ηz * fluxE_z)
+                    s_G[i, j,  _ρ] = MJ * (ηx * fluxρ_x + ηy * fluxρ_y + ηz * fluxρ_z)
+                    s_G[i, j,  _U] = MJ * (ηx * fluxU_x + ηy * fluxU_y + ηz * fluxU_z)
+                    s_G[i, j,  _V] = MJ * (ηx * fluxV_x + ηy * fluxV_y + ηz * fluxV_z)
+                    s_G[i, j,  _W] = MJ * (ηx * fluxW_x + ηy * fluxW_y + ηz * fluxW_z)
+                    s_G[i, j,  _E] = MJ * (ηx * fluxE_x + ηy * fluxE_y + ηz * fluxE_z)
 
-        #             r_Hρ = MJ * (ζx * fluxρ_x + ζy * fluxρ_y + ζz * fluxρ_z)
-        #             r_HU = MJ * (ζx * fluxU_x + ζy * fluxU_y + ζz * fluxU_z)
-        #             r_HV = MJ * (ζx * fluxV_x + ζy * fluxV_y + ζz * fluxV_z)
-        #             r_HW = MJ * (ζx * fluxW_x + ζy * fluxW_y + ζz * fluxW_z)
-        #             r_HE = MJ * (ζx * fluxE_x + ζy * fluxE_y + ζz * fluxE_z)
+                    r_Hρ = MJ * (ζx * fluxρ_x + ζy * fluxρ_y + ζz * fluxρ_z)
+                    r_HU = MJ * (ζx * fluxU_x + ζy * fluxU_y + ζz * fluxU_z)
+                    r_HV = MJ * (ζx * fluxV_x + ζy * fluxV_y + ζz * fluxV_z)
+                    r_HW = MJ * (ζx * fluxW_x + ζy * fluxW_y + ζz * fluxW_z)
+                    r_HE = MJ * (ζx * fluxE_x + ζy * fluxE_y + ζz * fluxE_z)
 
-        #             # one shared access per 10 flops
-        #             for n = 1:Nq
-        #                 Dkn = s_D[k, n]
+                    # one shared access per 10 flops
+                    for n = 1:Nq
+                        Dkn = s_D[k, n]
 
-        #                 r_rhsρ[n,i,j] += Dkn * r_Hρ
-        #                 r_rhsU[n,i,j] += Dkn * r_HU
-        #                 r_rhsV[n,i,j] += Dkn * r_HV
-        #                 r_rhsW[n,i,j] += Dkn * r_HW
-        #                 r_rhsE[n,i,j] += Dkn * r_HE
-        #             end
+                        r_rhsρ[n,i,j] += Dkn * r_Hρ
+                        r_rhsU[n,i,j] += Dkn * r_HU
+                        r_rhsV[n,i,j] += Dkn * r_HV
+                        r_rhsW[n,i,j] += Dkn * r_HW
+                        r_rhsE[n,i,j] += Dkn * r_HE
+                    end
 
-        #             r_rhsW[k,i,j] -= MJ * ρ * gravity
-        #         end
-        #     end
+                    r_rhsW[k,i,j] -= MJ * ρ * gravity
+                end
+            end
 
-        #     @synchronize
+            @synchronize
 
-        #     @loop for j in (1:Nq; threadIdx().y)
-        #         @loop for i in (1:Nq; threadIdx().x)
+            @loop for j in (1:Nq; threadIdx().y)
+                @loop for i in (1:Nq; threadIdx().x)
 
-        #             # loop of ξ-grid lines
-        #             for n = 1:Nq
-        #                 Dni = s_D[n, i]
-        #                 Dnj = s_D[n, j]
+                    # loop of ξ-grid lines
+                    for n = 1:Nq
+                        Dni = s_D[n, i]
+                        Dnj = s_D[n, j]
 
-        #                 r_rhsρ[k,i,j] += Dni * s_F[n, j, _ρ]
-        #                 r_rhsρ[k,i,j] += Dnj * s_G[i, n, _ρ]
+                        r_rhsρ[k,i,j] += Dni * s_F[n, j, _ρ]
+                        r_rhsρ[k,i,j] += Dnj * s_G[i, n, _ρ]
 
-        #                 r_rhsU[k,i,j] += Dni * s_F[n, j, _U]
-        #                 r_rhsU[k,i,j] += Dnj * s_G[i, n, _U]
+                        r_rhsU[k,i,j] += Dni * s_F[n, j, _U]
+                        r_rhsU[k,i,j] += Dnj * s_G[i, n, _U]
 
-        #                 r_rhsV[k,i,j] += Dni * s_F[n, j, _V]
-        #                 r_rhsV[k,i,j] += Dnj * s_G[i, n, _V]
+                        r_rhsV[k,i,j] += Dni * s_F[n, j, _V]
+                        r_rhsV[k,i,j] += Dnj * s_G[i, n, _V]
 
-        #                 r_rhsW[k,i,j] += Dni * s_F[n, j, _W]
-        #                 r_rhsW[k,i,j] += Dnj * s_G[i, n, _W]
+                        r_rhsW[k,i,j] += Dni * s_F[n, j, _W]
+                        r_rhsW[k,i,j] += Dnj * s_G[i, n, _W]
 
-        #                 r_rhsE[k,i,j] += Dni * s_F[n, j, _E]
-        #                 r_rhsE[k,i,j] += Dnj * s_G[i, n, _E]
-        #             end 
-        #         end 
-        #     end
-        # end # k
+                        r_rhsE[k,i,j] += Dni * s_F[n, j, _E]
+                        r_rhsE[k,i,j] += Dnj * s_G[i, n, _E]
+                    end 
+                end 
+            end
+        end # k
 
-        # @loop for j in (1:Nq; threadIdx().y)
-        #     @loop for i in (1:Nq; threadIdx().x)
-        #         for k in 1:Nq
-        #             MJI = vgeo[i, j, k, _MJI, e]
+        @loop for j in (1:Nq; threadIdx().y)
+            @loop for i in (1:Nq; threadIdx().x)
+                for k in 1:Nq
+                    MJI = vgeo[i, j, k, _MJI, e]
 
-        #             rhs[i, j, k, _U, e] += MJI*r_rhsU[k, i, j]
-        #             rhs[i, j, k, _V, e] += MJI*r_rhsV[k, i, j]
-        #             rhs[i, j, k, _W, e] += MJI*r_rhsW[k, i, j]
-        #             rhs[i, j, k, _ρ, e] += MJI*r_rhsρ[k, i, j]
-        #             rhs[i, j, k, _E, e] += MJI*r_rhsE[k, i, j]
-        #         end
-        #     end
-        # end
+                    rhs[i, j, k, _U, e] += MJI*r_rhsU[k, i, j]
+                    rhs[i, j, k, _V, e] += MJI*r_rhsV[k, i, j]
+                    rhs[i, j, k, _W, e] += MJI*r_rhsW[k, i, j]
+                    rhs[i, j, k, _ρ, e] += MJI*r_rhsρ[k, i, j]
+                    rhs[i, j, k, _E, e] += MJI*r_rhsE[k, i, j]
+                end
+            end
+        end
     end
     nothing
 end
@@ -570,8 +570,8 @@ function volumerhs_v3_1!(::Val{3},
 
                         r_rhsE[k,i,j] += Dni * s_F[n, j, _E]
                         r_rhsE[k,i,j] += Dnj * s_G[i, n, _E]
-                    end 
-                end 
+                    end
+                end
             end
         end # k
 
@@ -621,7 +621,7 @@ end
 
 
 function main()
-    # CUDAnative.timings!()
+    CUDAnative.timings!()
 
     parsed_args = parse_commandline()
 
@@ -651,16 +651,16 @@ function main()
     #
     # volumerhs_v2!
     #
-    #fill!(rhs, 0)
-    #@launch(CUDA(), threads=(N+1, N+1, N+1), blocks=nelem,
-    #      volumerhs_v2!(Val(3), Val(N), Val(nmoist), Val(ntrace),
-    #                    rhs, Q, vgeo, DFloat(grav), D, nelem))
-    #norm_v2 = norm(rhs)
-    # CUDAdrv.@profile for _ = 1:ntrials
-    #     @launch(CUDA(), threads=(N+1, N+1, N+1), blocks=nelem,
-    #           volumerhs_v2!(Val(3), Val(N), Val(nmoist), Val(ntrace), rhs,
-    #                         Q, vgeo, DFloat(grav), D, nelem))
-    # end
+    fill!(rhs, 0)
+    @launch(CUDA(), threads=(N+1, N+1, N+1), blocks=nelem,
+          volumerhs_v2!(Val(3), Val(N), Val(nmoist), Val(ntrace),
+                        rhs, Q, vgeo, DFloat(grav), D, nelem))
+    norm_v2 = norm(rhs)
+    CUDAdrv.@profile for _ = 1:ntrials
+         @launch(CUDA(), threads=(N+1, N+1, N+1), blocks=nelem,
+               volumerhs_v2!(Val(3), Val(N), Val(nmoist), Val(ntrace), rhs,
+                             Q, vgeo, DFloat(grav), D, nelem))
+    end
 
     #
     # volumerhs_v3!
@@ -677,31 +677,31 @@ function main()
                        DFloat(grav),
                        D,
                        nelem))
-    #norm_v3 = norm(rhs)
-    #CUDAdrv.@profile for _ = 1:ntrials
-    #    @launch(CUDA(), threads=(N+1, N+1), blocks=nelem, maxregs=255,
-    #          volumerhs_v3!(Val(3), Val(N), Val(nmoist), Val(ntrace), rhs,
-    #                        Q, vgeo, DFloat(grav), D, nelem))
-    #end
+    norm_v3 = norm(rhs)
+    CUDAdrv.@profile for _ = 1:ntrials
+        @launch(CUDA(), threads=(N+1, N+1), blocks=nelem, maxregs=255,
+              volumerhs_v3!(Val(3), Val(N), Val(nmoist), Val(ntrace), rhs,
+                            Q, vgeo, DFloat(grav), D, nelem))
+    end
 
-    ##
-    ## volumerhs_v3_1!
-    ##
-    #fill!(rhs, 0)
-    #@launch(CUDA(), threads=(N+1, N+1), blocks=nelem, maxregs=255,
-    #      volumerhs_v3_1!(Val(3), Val(N), Val(nmoist), Val(ntrace),
-    #                      rhs, Q, vgeo, DFloat(grav), D, nelem))
-    #norm_v3_1 = norm(rhs)
-    #CUDAdrv.@profile for _ = 1:ntrials
-    #    @launch(CUDA(), threads=(N+1, N+1), blocks=nelem, maxregs=255,
-    #          volumerhs_v3_1!(Val(3), Val(N), Val(nmoist), Val(ntrace),
-    #                          rhs, Q, vgeo, DFloat(grav), D, nelem))
-    #end
+    #
+    # volumerhs_v3_1!
+    #
+    fill!(rhs, 0)
+    @launch(CUDA(), threads=(N+1, N+1), blocks=nelem, maxregs=255,
+          volumerhs_v3_1!(Val(3), Val(N), Val(nmoist), Val(ntrace),
+                          rhs, Q, vgeo, DFloat(grav), D, nelem))
+    norm_v3_1 = norm(rhs)
+    CUDAdrv.@profile for _ = 1:ntrials
+        @launch(CUDA(), threads=(N+1, N+1), blocks=nelem, maxregs=255,
+              volumerhs_v3_1!(Val(3), Val(N), Val(nmoist), Val(ntrace),
+                              rhs, Q, vgeo, DFloat(grav), D, nelem))
+    end
 
-    #@assert norm_v2 ≈ norm_v3
-    #@assert norm_v2 ≈ norm_v3_1
+    @assert norm_v2 ≈ norm_v3
+    @assert norm_v2 ≈ norm_v3_1
 
-    #CUDAnative.timings()
+    CUDAnative.timings()
 
     nothing
 end
