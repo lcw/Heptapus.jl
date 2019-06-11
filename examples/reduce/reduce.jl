7d263c1344f58f4e189b82e023393d574573b062
Lucas C Wilcox <lucas@swirlee.com>
2019-06-11 21:40:30 +0000

    Use CuArrays/CUDAnative directly

--- examples/reduce/reduce.jl
+++ examples/reduce/reduce.jl
@@ -1,24 +1,21 @@
-using CuArrays, CUDAnative, GPUArrays
+using CUDAdrv, CuArrays, CUDAnative, GPUArrays
 CuArrays.allowscalar(false)
 import Base.Broadcast: Broadcasted, ArrayStyle
 using LazyArrays
 
-a = round.(rand(Float32, (3, 4)) * 100)
-b = round.(rand(Float32, (1, 4)) * 100)
-d_a = CuArray(a)
-d_b = CuArray(b)
-d_c = similar(d_a)
-d_c .= NaN
+function mycopyto_knl!(dest, bc)
+  I = CuArrays.@cuindex dest
+  @inbounds dest[I...] = bc[I...]
+  nothing
+end
 
 function mycopyto!(dest, bc::Broadcasted{Nothing})
   axes(dest) == axes(bc) || Broadcast.throwdm(axes(dest), axes(bc))
   bc′ = Broadcast.preprocess(dest, bc)
-  gpu_call(dest, (dest, bc′)) do state, dest, bc′
-    let I = CartesianIndex(@cartesianidx(dest))
-      @inbounds dest[I] = bc′[I]
-    end
-    return
-  end
+  dev = CUDAdrv.device()
+  thr = attribute(dev, CUDAdrv.MAX_THREADS_PER_BLOCK)
+  blk = length(dest) ÷ thr + 1
+  @cuda blocks=blk threads=thr mycopyto_knl!(dest, bc)
   return dest
 end
 
@@ -26,6 +23,12 @@ end
 # `fill!` in general for all `GPUDestArray` so we just go straight to the fallback
 @inline mycopyto!(dest, bc::Broadcasted{ArrayStyle{CuArray}}) =
     mycopyto!(dest, convert(Broadcasted{Nothing}, bc))
+
+a = round.(rand(Float32, (3, 4)) * 100)
+b = round.(rand(Float32, (1, 4)) * 100)
+d_a = CuArray(a)
+d_b = CuArray(b)
+d_c = similar(d_a)
 
 mycopyto!(d_c, @~ d_a .* d_b)
 
