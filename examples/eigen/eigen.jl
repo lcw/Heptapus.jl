4c3879fc111c7b7b971d7260770b0ff90c6181e5
Lucas C Wilcox <lucas@swirlee.com>
2019-09-06 14:15:35 -0700

    Fix SofAs and add GPUifyLoops

--- examples/eigen/eigen.jl
+++ examples/eigen/eigen.jl
@@ -1,5 +1,10 @@
 using LinearAlgebra, GPUifyLoops, StaticArrays, StructArrays, Random
 
+@static if Base.find_package("CuArrays") !== nothing
+    using CuArrays, CUDAnative
+end
+
+
 Random.seed!(4)
 
 """
@@ -12,9 +17,9 @@ Implementation of the 2 x 2 eignvalue and eigenvector solver found in:
   }
 """
 @inline function cfbeigen(A::SHermitianCompact{2,T,3}) where {T <: Real}
-    α₁ = A[1, 1]
-    α₂ = A[2, 2]
-    β = A[2, 1]
+    @inbounds α₁ = A[1, 1]
+    @inbounds α₂ = A[2, 2]
+    @inbounds β = A[2, 1]
 
     δ = (α₁ - α₂)/2
     h = hypot(δ, β)
@@ -44,9 +49,42 @@ Implementation of the 2 x 2 eignvalue and eigenvector solver found in:
 end
 
 # initialize with random symmetric matrices
-s = StructArray(SHermitianCompact(@SVector(rand(3))) for i=1:5)
+N = 5
+T = Float64
 
-F1 = eigen(s[1])
-F2 = cfbeigen(s[1])
+SH = SHermitianCompact{2, T, 3}
+As = StructArray{SH}(undef, N, unwrap = t -> t <: Union{SVector,Tuple})
+for i = 1:N
+    As[i] = SHermitianCompact(@SVector(rand(3)))
+end
 
-@info "Test" F1 F2
+SM = SMatrix{2, 2, T, 4}
+Vs = StructArray{SM}(undef, N, unwrap = t -> t <: Union{NTuple})
+
+SV = SVector{2, T}
+λs = StructArray{SV}(undef, N, unwrap = t -> t <: Union{NTuple})
+
+function kernel(As, Vs, λs)
+    @inbounds @loop for i in (1:size(As,1);
+                              (blockIdx().x-1)*blockDim().x + threadIdx().x)
+        F = cfbeigen(As[i])
+        Vs[i] = F.vectors
+        λs[i] = F.values
+    end
+    nothing
+end
+
+@launch CPU() kernel(As, Vs, λs)
+
+@static if Base.find_package("CuArrays") !== nothing
+    threads = 1024
+    blocks = cld(size(As,1),threads)
+    replace_storage(CuArray, As)
+    replace_storage(CuArray, Vs)
+    replace_storage(CuArray, λs)
+    @launch(CUDA(), threads=threads, blocks=blocks, kernel(As, Vs, λs))
+end
+
+@show As
+@show Vs
+@show λs
