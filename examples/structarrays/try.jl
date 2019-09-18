a9723c8062c24b397cf9e5417bbd6ce53e8b3942
Lucas C Wilcox <lucas@swirlee.com>
2019-09-18 16:56:07 +0000

    Example of what I want to work

--- examples/structarrays/try.jl
+++ examples/structarrays/try.jl
@@ -1,4 +1,27 @@
-using CuArrays, StaticArrays, StructArrays
+using CUDAnative, CuArrays, StaticArrays, StructArrays
 
-aofs = [SHermitianCompact(@SVector(rand(3))) for i=1:3]
-sofa = StructArray(aofs, unwrap = t -> t <: Union{SVector,Tuple})
+CUDAnative.cudaconvert(s::StructArray) = replace_storage(cudaconvert, s)
+
+# c = [(a=SHermitianCompact(@SVector(rand(3))), b=(@SVector(rand(2)))) for i=1:5]
+
+c = [SHermitianCompact(@SVector(rand(3))) for i=1:5]
+d = StructArray(c, unwrap = t -> t <: Union{SHermitianCompact,SVector,Tuple})
+
+dd = replace_storage(CuArray, d)
+de = similar(dd)
+
+@show typeof(dd)
+
+function kernel!(dest, src)
+  i = (blockIdx().x-1)*blockDim().x + threadIdx().x
+  if i <= length(dest)
+    dest[i].a = src[i].a
+    dest[i].b = src[i].b
+  end
+  return nothing
+end
+
+threads = 1024
+blocks = cld(length(dd),threads)
+
+@cuda threads=threads blocks=blocks kernel!(de, dd)
