166d59f1b6dc434e6d964072eeb42c857cfb5a8f
Lucas C Wilcox <lucas@swirlee.com>
2019-09-23 17:23:03 +0000

    Minor cleanup

--- examples/structarrays/lutils.jl
+++ examples/structarrays/lutils.jl
@@ -5,12 +5,12 @@ using StructArrays
 import StructArrays: staticschema, map_params, astuple, _map_params
 using Base: tuple_type_cons, tuple_type_head, tuple_type_tail, tail
 
-function sstuple(::Type{<:NTuple{N, Any}}) where {N}
+function _sstuple(::Type{<:NTuple{N, Any}}) where {N}
     ntuple(j->Symbol(j), N)
 end
 
-function sstuple(::Type{NT}) where {NT<:NamedTuple}
-    _map_params(x->sstuple(staticschema(x)), NT)
+function _sstuple(::Type{NT}) where {NT<:NamedTuple}
+    _map_params(x->_sstuple(staticschema(x)), NT)
 end
 
 function _getcolproperties!(exprs, s, es=[])
@@ -25,9 +25,28 @@ end
 
 using StaticArrays
 c = [(a=SHermitianCompact(@SVector(rand(3))), b=(@SVector(rand(2)))) for i=1:5]
-d = d = StructArray(c, unwrap = t -> t <: Union{SHermitianCompact,SVector,Tuple})
+d = StructArray(c, unwrap = t -> t <: Union{SHermitianCompact,SVector,Tuple})
 b = []
 S = StructArrays.staticschema(typeof(d))
-s = sstuple(S)
+s = _sstuple(S)
 _getcolproperties!(b, s)
 @show b
+
+function f(columnsproperties, xs)
+  exprs = Expr[]
+  for col in columnsproperties
+    args = Expr[]
+    for prop in col
+      if length(args) == 0
+        args = [Expr(:call, :_getproperty, :(getfield(xs, $j)), prop) for j in 1:length(xs)]
+      else
+        for j in 1:length(xs)
+          args[j] = Expr(:call, :_getproperty, args[j], prop)
+        end
+      end
+    end
+    push!(exprs, Expr(:call, :f, args...))
+  end
+  push!(exprs, :(return nothing))
+  return Expr(:block, exprs...)
+end
