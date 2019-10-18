9c1f740288a4b6c2061774f3f3784ad21831d5c3
Lucas C Wilcox <lucas@swirlee.com>
2019-10-18 11:37:20 -0700

    Remove broken or unused stuff
    It turns out that we are not going to be able to pass banded matrices to
    the GPU since they are not isbits.  We also will not be able to use the
    GPU sparse matrix stuff because solvers only work for a single
    right-hand side.

--- examples/banded/try.jl
+++ examples/banded/try.jl
@@ -1,57 +1,6 @@
 using BandedMatrices, LinearAlgebra, UnicodePlots
-
-using GPUifyLoops
-using CUDAnative
-
-using CuArrays
-using CuArrays.CUSPARSE
-using CuArrays.CUSOLVER
-
-import Adapt
-Adapt.adapt_storage(to::Type{<:CuArray}, b::BandedMatrix) = BandedMatrices._BandedMatrix(Adapt.adapt(to, b.data), size(b, 2), b.l, b.u)
-Adapt.adapt_storage(to, b::BandedMatrix) = BandedMatrices._BandedMatrix(Adapt.adapt(to, b.data), size(b, 2), b.l, b.u)
-
+using GPUifyLoops, CUDAnative, CuArrays
 using Test
-
-## let
-## 
-##   m = 15
-##   n = 15
-##   o = 1000
-##   p = 3
-##   q = 2
-## 
-##   A = brand(Float64, m, n, p, q)
-##   B = rand(Float64, m, o)
-##   X = A\B
-##   A * X ≈ B
-## 
-##   F = lu(A)
-## 
-##   X = F\B
-##   @test A * X ≈ B
-## 
-##   ## dA = BandedMatrices._BandedMatrix(CuArray(A.data), size(A, 2), A.l, A.u)
-##   ## L = CuArray(F.L)
-##   ## U = BandedMatrices._BandedMatrix(CuArray(F.U.data), size(F.U, 2), F.U.l, F.U.u)
-## 
-##   d_A = CuArray(A)
-##   d_B = CuArray(B)
-## 
-##   d_A, d_ipiv = CUSOLVER.getrf!(d_A)
-##   d_B         = CUSOLVER.getrs!('N', d_A, d_ipiv, d_B)
-## 
-## 
-##   @test Array(d_B) ≈ A\B
-## 
-##   ## dA = CuSparseMatrixCSR(sA)
-##   ## dB = CuArray(B)
-##   ## dX = similar(dB)
-##   ## tol = 1e-6
-##   ## dX = CUSOLVER.csrlsvlu!(dA,dB,dX,tol,one(Cint),'O')
-##   ## dX = CUSOLVER.csrlsvqr!(dA,dB,dX,tol,one(Cint),'O')
-## 
-## end
 
 function forward!(A, B)
     @inbounds @loop for i in (1:size(A,1);
