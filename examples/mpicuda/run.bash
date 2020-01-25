c0abd548f6638736c0ff3f4db36fdf59cc3d4a3e
Lucas C Wilcox <lucas@swirlee.com>
2020-01-24 16:55:25 -0800

    Add slurm batch script from hamming

---
+++ examples/mpicuda/run.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=test_mpi_cudanative
+#SBATCH --output=out_%j.txt
+#SBATCH --partition=allgpu
+#SBATCH --gres=gpu:titanv:2
+
+#SBATCH --ntasks=2
+#SBATCH --time=10:00
+#SBATCH --mem=12GB
+
+source /etc/profile
+module load compile/gcc/7.2.0 openmpi/3.0.0 lib/cuda/10.1.243
+
+srun nvprof -o "timeline_rank_%q{SLURM_PROCID}_job_%q{SLURM_JOBID}" \
+            --context-name "MPI Rank %q{SLURM_PROCID}" \
+            --process-name "MPI Rank %q{SLURM_PROCID}" \
+            --annotate-mpi openmpi \
+            julia --project=. try.jl
