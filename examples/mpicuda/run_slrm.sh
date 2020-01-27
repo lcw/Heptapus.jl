d5e783944798fcd269c6a93f2f2b40faa09221c0
Lucas C Wilcox <lucas@swirlee.com>
2020-01-27 15:58:17 -0800

    Update slrm script with hamming configuration
    This is the configuration that works on the hamming cluster.

--- examples/mpicuda/run_slrm.sh
+++ examples/mpicuda/run_slrm.sh
@@ -9,11 +9,16 @@
 #SBATCH --time=10:00
 #SBATCH --mem=12GB
 
+# Batch this job with `sbatch run_slrm.sh`
+
 source /etc/profile
 module load compile/gcc/7.2.0 openmpi/3.0.0 lib/cuda/10.1.243
 
-srun nvprof -o "timeline_rank_%q{SLURM_PROCID}_job_%q{SLURM_JOBID}" \
-            --context-name "MPI Rank %q{SLURM_PROCID}" \
-            --process-name "MPI Rank %q{SLURM_PROCID}" \
-            --annotate-mpi openmpi \
-            julia --project=. try.jl
+# If launching with `srun` then replace `OMPI_COMM_WORLD_RANK` with
+# `SLURM_PROCID`.
+
+mpirun nvprof -o "timeline_rank_%q{OMPI_COMM_WORLD_RANK}_job_%q{SLURM_JOBID}" \
+              --context-name "MPI Rank %q{OMPI_COMM_WORLD_RANK}" \
+              --process-name "MPI Rank %q{OMPI_COMM_WORLD_RANK}" \
+              --annotate-mpi openmpi \
+              julia --project=. try.jl
