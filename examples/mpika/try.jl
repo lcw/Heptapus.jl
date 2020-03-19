42d41edd4bb8ad6c0788cc6657be8c17f9d1611a
Lucas C Wilcox <lucas@swirlee.com>
2020-03-19 11:55:41 -0700

    Broken example

--- examples/mpika/try.jl
+++ examples/mpika/try.jl
@@ -1,110 +1,93 @@
-using KernelAbstractions
 using CuArrays
-using CUDAnative
-using CUDAdrv
+using KernelAbstractions
 using MPI
-using KernelAbstractions: CudaEvent, Event
 
-@kernel function kernel!(c, @Const(a), @Const(b))
-  i = @index(Global)
+device(A) = typeof(A) <: Array ? CPU() : CUDA()
 
-  @inbounds a_val = a[i]
-  @inbounds b_val = b[i]
-  @inbounds c_val = c[i]
-
-  for j in 1:99
-    a_val = a_val * b_val + c_val
-    b_val = a_val * b_val + c_val
-    c_val = a_val * b_val + c_val
-  end
-
-  @inbounds c[i] = a_val * b_val + c_val
-
-  return
+function mpiyield()
+    MPI.Iprobe(MPI.MPI_ANY_SOURCE, MPI.MPI_ANY_TAG, MPI.COMM_WORLD)
+    yield()
 end
 
-@kernel function mpikernel!(c, b, src_rank, dst_rank, comm)
-  rreq = MPI.Irecv!(c, src_rank, 222, comm)
-  sreq = MPI.Isend(b, dst_rank, 222, comm)
-  stats = MPI.Waitall!([sreq, rreq])
+function __Irecv!(request, buf, src, tag, comm)
+    @info "Recv" buf src tag
+    request[1] = MPI.Irecv!(buf, src, tag, comm)
 end
 
-function pin!(a)
-  ad = Mem.register(Mem.Host, pointer(a), sizeof(a))
-  finalizer(_ -> Mem.unregister(ad), a)
+function __Isend!(request, buf, dst, tag, comm)
+    @info "Send" buf dst tag
+    request[1] = MPI.Isend!(buf, dst, tag, comm)
 end
 
-function recordevent(stream)
-  event = CuEvent(CUDAdrv.EVENT_DISABLE_TIMING)
-  CUDAdrv.record(event, stream)
-  return CudaEvent(event)
-end
-
-function async_copyto!(destptr, srcptr, N::Integer; stream=CuDefaultStream(),
-                       dependencies=nothing)
-  if dependencies isa Event
-    dependencies = (dependencies,)
-  end
-  if dependencies !== nothing
-    for event in dependencies
-      if event isa CudaEvent
-        CUDAdrv.wait(event.event, stream)
-      else
-        KernelAbstractions.wait(event)
-      end
+function __testall!(requests; dependencies=nothing)
+    @info "Testall" requests
+    done = false
+    while !done
+        done, _ = MPI.Testall!(requests)
+        yield()
     end
-  end
-
-  unsafe_copyto!(destptr, srcptr, N, async=true, stream=stream)
-
-  return recordevent(stream)
 end
 
+function __print_buf(buf)
+    @info "printing buf" buf
+end
+
+function exchange!(d_send_buf, h_send_buf, d_recv_buf, h_recv_buf, src_rank,
+                   dst_rank, send_request, recv_request, comm;
+                   dependencies=nothing)
+
+    event = Event(__Irecv!, recv_request, h_recv_buf, src_rank, 666, comm;
+                  dependencies = dependencies)
+    event = Event(__testall!, recv_request; dependencies = event)
+    event = Event(__print_buf, h_recv_buf; dependencies = event)
+    recv_event = async_copy!(device(d_recv_buf), d_recv_buf, h_recv_buf;
+                             dependencies = event)
+
+    event = async_copy!(device(d_send_buf), h_send_buf, d_send_buf;
+                        dependencies = dependencies)
+    event = Event(__Isend!, send_request, h_send_buf, dst_rank, 666, comm;
+                  dependencies = event)
+    send_event = Event(__testall!, send_request; dependencies = event)
+
+    return recv_event, send_event
+end
 
 function main()
-  if !MPI.Initialized()
-    MPI.Init()
-  end
-  comm = MPI.COMM_WORLD
-  local_comm = MPI.Comm_split_type(comm, MPI.MPI_COMM_TYPE_SHARED,
-                                   MPI.Comm_rank(comm))
-  device!(MPI.Comm_rank(local_comm) % length(devices()))
-  copystream = CuStream(CUDAdrv.STREAM_NON_BLOCKING)
-  CuArrays.allowscalar(false)
+    if !MPI.Initialized()
+        MPI.Init()
+    end
+    comm = MPI.COMM_WORLD
+    MPI.Barrier(comm)
 
-  synchronize(copystream)
-  MPI.Barrier(comm)
+    dst_rank = mod(MPI.Comm_rank(comm)+1, MPI.Comm_size(comm))
+    src_rank = mod(MPI.Comm_rank(comm)-1, MPI.Comm_size(comm))
 
-  dst_rank = mod(MPI.Comm_rank(comm)+1, MPI.Comm_size(comm))
-  src_rank = mod(MPI.Comm_rank(comm)-1, MPI.Comm_size(comm))
+    T = Int64
+    M = 10
 
-  T = Float32
-  M = 1024
-  N = M*100
+    d_send_buf = CuArrays.zeros(T, M)
+    d_recv_buf = CuArrays.zeros(T, M)
+    h_send_buf = zeros(T, M)
+    h_recv_buf = zeros(T, M)
 
-  A = CuArray(rand(T, M, N))
-  B = CuArray(rand(T, M, N))
-  C = CuArray(rand(T, M, N))
-  d = CuArray(rand(T, M))
+    fill!(d_send_buf, MPI.Comm_rank(comm))
+    fill!(d_recv_buf, -1)
+    fill!(h_send_buf, -1)
+    fill!(h_recv_buf, -1)
 
-  b = Array{T}(undef, M)
-  c = Array{T}(undef, M)
-  pin!(b)
-  pin!(c)
+    send_request = fill(MPI.REQUEST_NULL, 1)
+    recv_request = fill(MPI.REQUEST_NULL, 1)
 
-  len = length(A)
-  threads = min(len, 1024)
-  blocks = len ÷ threads
+    event = Event(device(d_send_buf))
+    recv_event, send_event = exchange!(d_send_buf, h_send_buf, d_recv_buf,
+                                       h_recv_buf, src_rank, dst_rank,
+                                       send_request, recv_request, comm;
+                                       dependencies = event)
+    wait(CPU(), recv_event, yield)
+    wait(CPU(), send_event, yield)
 
-  compevent = recordevent(copystream)
-  copyevent = recordevent(copystream)
-  for j = 1:100
-    copyevent = async_copyto!(pointer(b), pointer(B), M, stream=copystream, dependencies=compevent)
-    compevent = kernel!(CUDA(), 256)(C, A, B, ndrange=length(C), dependencies=compevent)
-    copyevent = mpikernel!(CPU(), 1)(c, b, src_rank, dst_rank, comm, ndrange=1, dependencies=copyevent)
-    copyevent = async_copyto!(pointer(d), pointer(c), M, stream=copystream, dependencies=copyevent)
-    copyevent = async_copyto!(pointer(C), pointer(d), M, stream=copystream, dependencies=copyevent)
-    compevent = kernel!(CUDA(), 256)(C, A, B, ndrange=length(C), dependencies=copyevent)
-  end
+    @assert all(d_recv_buf .== src_rank)
+
 end
+
 main()
