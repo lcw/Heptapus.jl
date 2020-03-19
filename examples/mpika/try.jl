f4b456fac320a83bc89bd21e3b20919ed3eb3108
Lucas C Wilcox <lucas@swirlee.com>
2020-03-19 13:09:51 -0700

    Second example

--- examples/mpika/try.jl
+++ examples/mpika/try.jl
@@ -1,4 +1,5 @@
 using CuArrays
+using CUDAdrv
 using KernelAbstractions
 using MPI
 
@@ -32,21 +33,20 @@ function __print_buf(buf)
     @info "printing buf" buf
 end
 
-function exchange!(d_send_buf, h_send_buf, d_recv_buf, h_recv_buf, src_rank,
-                   dst_rank, send_request, recv_request, comm;
-                   dependencies=nothing)
+function exchange!(h_send_buf, d_recv_buf, h_recv_buf, src_rank, dst_rank,
+                   send_request, recv_request, comm; dependencies=nothing)
 
     event = Event(__Irecv!, recv_request, h_recv_buf, src_rank, 666, comm;
                   dependencies = dependencies)
     event = Event(__testall!, recv_request; dependencies = event)
-    event = Event(__print_buf, h_recv_buf; dependencies = event)
+    # Works:
+    # recv_event = Event(copyto!, d_recv_buf, h_recv_buf; dependencies = event)
+    # Fails:
     recv_event = async_copy!(device(d_recv_buf), d_recv_buf, h_recv_buf;
                              dependencies = event)
 
-    event = async_copy!(device(d_send_buf), h_send_buf, d_send_buf;
-                        dependencies = dependencies)
     event = Event(__Isend!, send_request, h_send_buf, dst_rank, 666, comm;
-                  dependencies = event)
+                  dependencies = dependencies)
     send_event = Event(__testall!, send_request; dependencies = event)
 
     return recv_event, send_event
@@ -65,29 +65,26 @@ function main()
     T = Int64
     M = 10
 
-    d_send_buf = CuArrays.zeros(T, M)
     d_recv_buf = CuArrays.zeros(T, M)
     h_send_buf = zeros(T, M)
     h_recv_buf = zeros(T, M)
 
-    fill!(d_send_buf, MPI.Comm_rank(comm))
     fill!(d_recv_buf, -1)
-    fill!(h_send_buf, -1)
+    fill!(h_send_buf, MPI.Comm_rank(comm))
     fill!(h_recv_buf, -1)
 
     send_request = fill(MPI.REQUEST_NULL, 1)
     recv_request = fill(MPI.REQUEST_NULL, 1)
 
-    event = Event(device(d_send_buf))
-    recv_event, send_event = exchange!(d_send_buf, h_send_buf, d_recv_buf,
-                                       h_recv_buf, src_rank, dst_rank,
-                                       send_request, recv_request, comm;
-                                       dependencies = event)
+    synchronize()
+
+    recv_event, send_event = exchange!(h_send_buf, d_recv_buf, h_recv_buf,
+                                       src_rank, dst_rank, send_request,
+                                       recv_request, comm)
     wait(CPU(), recv_event, yield)
     wait(CPU(), send_event, yield)
 
     @assert all(d_recv_buf .== src_rank)
-
 end
 
 main()
